bc1634de3bcd8db78ec34bb88a328048c9263c41
AhmadR3zA <ahmad.r1911@gmail.com>
2021-09-20 00:44:45 +0430

    start card project
    create main file

--- Demo/cards/main.go
+++ Demo/cards/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//var card string = "Aces of Spades"
+	//card := "Aces of Spades" //other way declare variable
+	card := newCard()
+	fmt.Println(card)
+}
+
+//define function
+func newCard() string {
+
+	return "Five of Diamonds"
 }
